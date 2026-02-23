e2c7a3eb3f0cc7acc57d262d61e12cf85f2287db
JengaCube <gg1128k@gre.ac.uk>
2026-02-23 09:07:44 +0000

    temp: removed print debugging

--- Fine-Tuning.py
+++ Fine-Tuning.py
@@ -1,15 +1,10 @@
 import pandas as pd
-import numpy as np
 import torch
 import transformers
 from transformers.pipelines.base import Dataset
 
-print("1")
-
 df_train = pd.read_csv('archive/train.csv', encoding="utf-8")
 df_test = pd.read_csv('archive/test.csv', encoding="utf-8")
-
-print("2")
 
 not_labels = ['id', 'comment_text']
 
@@ -21,24 +16,14 @@ labels_list_train = df_labels_train.values.tolist()
 df_labels_test = pd.read_csv('archive/test_labels.csv', encoding="utf-8")
 labels_list_test = df_labels_test.values.tolist()
 
-print("3")
-
 train_comments = df_train['comment_text'].tolist()
-
-print("4")
 
 test_comments = df_test['comment_text'].tolist()
 
-print("5")
-
 tokenizer = transformers.AutoTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
-
-print("6")
 
 train_tokenized = tokenizer(train_comments, padding="max_length", truncation=True, max_length=128)
 test_tokenized = tokenizer(test_comments, padding="max_length", truncation=True, max_length=128)
-
-print("7")
 
 class TextClassifierDataset(Dataset):
     def __init__(self, encodings, labels):
@@ -53,23 +38,14 @@ class TextClassifierDataset(Dataset):
         item["labels"] = torch.tensor(self.labels[idx])
         return item
 
-print("8")
-
 train_dataset = TextClassifierDataset(train_tokenized, labels_list_train)
 test_dataset = TextClassifierDataset(test_tokenized, labels_list_test)
 
-print("9")
-
 model = transformers.AutoModelForSequenceClassification.from_pretrained("bert-base-uncased", problem_type="multi_label_classification", num_labels=6)
-
-print("10")
 
 training_arguments = transformers.TrainingArguments(output_dir=".", eval_strategy="epoch", per_device_train_batch_size=2, per_device_eval_batch_size=2)
 
-print("11")
-
 trainer = transformers.Trainer(model=model, args=training_arguments, train_dataset=train_dataset, eval_dataset=test_dataset)
 
-print("12")
 
 trainer.train()
