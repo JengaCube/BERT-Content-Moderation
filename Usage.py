4b46fa5dd3396f71007d0966b041c9d7d44c0318
JengaCube <gg1128k@gre.ac.uk>
2026-04-06 12:14:54 +0100

    feat: Outputs the results of the BERT Model and outputs them into a csv file

--- Usage.py
+++ Usage.py
@@ -1,12 +1,18 @@
 import torch
 import transformers
-
+import pandas as pd
 
 tokenizer = transformers.AutoTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
 
-model = torch.load('Saved Model/model.safetensors')
+model = transformers.AutoModelForSequenceClassification.from_pretrained("SavedModel/Model.pth")
 
-text_list = ["Hello World!"]
+df_test = pd.read_csv('archive/test.csv', encoding="UTF-8")
+
+text_list = df_test["comment_text"].tolist()
+
+headers = ["comment", "toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
+
+df = pd.DataFrame(columns=headers)
 
 model.eval()
 for text in text_list:
@@ -14,12 +20,15 @@ for text in text_list:
 
     with torch.no_grad():
         outputs = model(**inputs)
-
     logits = outputs.logits
     probabilities = torch.sigmoid(logits)
+    probabilities = probabilities.tolist()
+    flattened = [item for sublist in probabilities for item in sublist]
+    flattened.insert(0, text)
+    new_row = pd.DataFrame([flattened], columns=df.columns)
+    df = pd.concat([df, new_row], ignore_index=True)
 
-    all_probabilities = []
 
-    all_probabilities.append(probabilities)
 
-print(all_probabilities)
+print(df)
+df.to_csv('output.csv', index=False)
