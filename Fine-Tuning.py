e8229582e0bd5647d98605986e3ad852c096b9ca
jengacube <gabrielglod928@gmail.com>
2026-04-14 21:44:29 +0100

    Feat: Removed redundant test dataset from script

--- Fine-Tuning.py
+++ Fine-Tuning.py
@@ -4,7 +4,6 @@ import transformers
 from transformers.pipelines.base import Dataset
 
 df_train = pd.read_csv('archive/train.csv', encoding="utf-8")
-df_test = pd.read_csv('archive/test.csv', encoding="utf-8")
 
 not_labels = ['id', 'comment_text']
 
@@ -14,18 +13,13 @@ df_labels_train = df_train[label_columns]
 labels_list_train = df_labels_train.values.tolist()
 labels_list_train = torch.tensor(labels_list_train).float()
 
-df_labels_test = pd.read_csv('archive/test_labels.csv', encoding="utf-8")
-labels_list_test = df_labels_test.values.tolist()
-labels_list_test = torch.tensor(labels_list_test).float()
 
 train_comments = df_train['comment_text'].tolist()
 
-test_comments = df_test['comment_text'].tolist()
 
 tokenizer = transformers.AutoTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
 
 train_tokenized = tokenizer(train_comments, padding="max_length", truncation=True, max_length=128)
-test_tokenized = tokenizer(test_comments, padding="max_length", truncation=True, max_length=128)
 
 class TextClassifierDataset(Dataset):
     def __init__(self, encodings, labels):
@@ -41,7 +35,6 @@ class TextClassifierDataset(Dataset):
         return item
 
 train_dataset = TextClassifierDataset(train_tokenized, labels_list_train)
-test_dataset = TextClassifierDataset(test_tokenized, labels_list_test)
 
 model = transformers.AutoModelForSequenceClassification.from_pretrained("bert-base-uncased", problem_type="multi_label_classification", num_labels=6)
 
@@ -55,7 +48,7 @@ num_train_epochs=4,
 weight_decay=0.01
 )
 
-trainer = transformers.Trainer(model=model, args=training_arguments, train_dataset=train_dataset, eval_dataset=test_dataset)
+trainer = transformers.Trainer(model=model, args=training_arguments, train_dataset=train_dataset)
 
 
 trainer.train()
