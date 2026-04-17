7605743b7acf29929179e729f3bf0945f19123c9
jengacube <gabrielglod928@gmail.com>
2026-04-17 15:05:21 +0100

    Fix: LABELS list was removed on accident. Readded in this version

--- Metrics.py
+++ Metrics.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.metrics import classification_report, confusion_matrix
 
 def metrics(preds, y_true):
+    LABELS = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
+
     y_pred = pd.DataFrame(preds, columns=LABELS)
 
     print(classification_report(y_true, y_pred.values, target_names=LABELS, zero_division=0))
