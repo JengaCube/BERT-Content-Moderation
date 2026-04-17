02df603a824a29b633ecd77c6faa132bfc63b6cb
jengacube <gabrielglod928@gmail.com>
2026-04-17 15:08:57 +0100

    Feat: Metrics function added for evaluation. Cleaned up output saving

--- Usage.py
+++ Usage.py
@@ -1,34 +1,49 @@
 import torch
 import transformers
 import pandas as pd
+from Metrics import metrics
 
 tokenizer = transformers.AutoTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
 
 model = transformers.AutoModelForSequenceClassification.from_pretrained("SavedModel/Model.pth")
 
 df_test = pd.read_csv('archive/test.csv', encoding="UTF-8")
+df_labels = pd.read_csv("archive/test_labels.csv")
 
-text_list = df_test["comment_text"].tolist()
+LABELS = ["toxic","severe_toxic","obscene","threat","insult","identity_hate"]
+
+mask = ~(df_labels[LABELS] == -1).any(axis=1)
+text_list = df_test.loc[mask, "comment_text"].fillna("").tolist()
+y_true = df_labels.loc[mask, LABELS].values
 
 headers = ["comment", "toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
 
 df = pd.DataFrame(columns=headers)
 
 model.eval()
+preds = []
+rows = []
+
+threshold = 0.5
+
 for text in text_list:
     inputs = tokenizer(text, padding=True, truncation=True, return_tensors="pt")
 
     with torch.no_grad():
         outputs = model(**inputs)
-    logits = outputs.logits
-    probabilities = torch.sigmoid(logits)
-    probabilities = probabilities.tolist()
-    flattened = [item for sublist in probabilities for item in sublist]
-    flattened.insert(0, text)
-    new_row = pd.DataFrame([flattened], columns=df.columns)
-    df = pd.concat([df, new_row], ignore_index=True)
+
+    probs = torch.sigmoid(outputs.logits).cpu().numpy()[0]
 
 
+    binary_preds = (probs >= threshold).astype(int)
+    preds.append(binary_preds)
 
+    row = [text] + probs.tolist()
+    rows.append(row)
+
+
+df = pd.DataFrame(rows, columns=["comment_text"] + LABELS)
 print(df)
 df.to_csv('Outputs/output.csv', index=False)
+
+metrics(preds, y_true)
