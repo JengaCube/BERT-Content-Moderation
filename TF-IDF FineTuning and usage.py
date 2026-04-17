d51cef90dcf2645650da8fba826d23da50834f9a
jengacube <gabrielglod928@gmail.com>
2026-04-17 15:07:04 +0100

    Feat: Multiple changes made: -Training dataset is not split -Testing dataset added -Metrics function added for evaluation

--- TF-IDF FineTuning and usage.py
+++ TF-IDF FineTuning and usage.py
@@ -1,24 +1,34 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.linear_model import LogisticRegression
+from Metrics import metrics
+
 
 data = pd.read_csv("archive/train.csv")
 
-X = data["comment_text"]
-y = data[["toxic","severe_toxic","obscene","threat","insult","identity_hate"]]
+LABELS = ["toxic","severe_toxic","obscene","threat","insult","identity_hate"]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X = data["comment_text"]
+y = data[LABELS]
+
+df_test = pd.read_csv('archive/test.csv', encoding="UTF-8")
+df_labels = pd.read_csv("archive/test_labels.csv")
+
+mask = ~(df_labels[LABELS] == -1).any(axis=1)
+text_list = df_test.loc[mask, "comment_text"].fillna("").tolist()
+y_true = df_labels.loc[mask, LABELS].values
 
 vectorizer = TfidfVectorizer(max_features=20000, stop_words="english")
-X_train_tfidf = vectorizer.fit_transform(X_train)
-X_test_tfidf = vectorizer.transform(X_test)
+train_vectorised = vectorizer.fit_transform(X)
+test_vectorised = vectorizer.fit_transform(text_list)
 
 model = OneVsRestClassifier(LogisticRegression(max_iter=1000))
-model.fit(X_train_tfidf, y_train)
+model.fit(train_vectorised, y)
 
-predictions = model.predict(X_test_tfidf)
+predictions = model.predict(test_vectorised)
 
 output = pd.DataFrame(predictions, columns=y.columns)
 output.to_csv("Outputs/predictions.csv", index=False)
+
+metrics(predictions, y_true)
