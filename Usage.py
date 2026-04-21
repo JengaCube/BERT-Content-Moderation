f72b3c40882d6e6d687777d49a0ac9a7fb7a6ecd
jengacube <gabrielglod928@gmail.com>
2026-04-22 00:55:23 +0100

    Feat: Updated i/o for Manual input mode

--- Usage.py
+++ Usage.py
@@ -12,24 +12,23 @@ model = transformers.AutoModelForSequenceClassification.from_pretrained("SavedMo
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
-print("(1) Metrics eval")
-print("(2) Manual Input")
-mode = int(input(""))
-
 LABELS = ["toxic","severe_toxic","obscene","threat","insult","identity_hate"]
 
 headers = ["comment", "toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
-if mode == 1:
-    df_test = pd.read_csv('archive/test.csv', encoding="UTF-8")
-    df_labels = pd.read_csv("archive/test_labels.csv")
-    mask = ~(df_labels[LABELS] == -1).any(axis=1)
-    text_list = df_test.loc[mask, "comment_text"].fillna("").tolist()
-    y_true = df_labels.loc[mask, LABELS].values
-elif mode == 2:
-    text_list = str(input("Please enter you model prompt"))
-else:
-    print("Invalid mode chosen. Please rerun the script.")
-    sys.exit()
+
+mode = 3
+while mode >= 3:
+    print("(1) Metrics eval")
+    print("(2) Manual Input")
+    mode = int(input(""))
+    if mode == 1:
+        df_test = pd.read_csv('archive/test.csv', encoding="UTF-8")
+        df_labels = pd.read_csv("archive/test_labels.csv")
+        mask = ~(df_labels[LABELS] == -1).any(axis=1)
+        text_list = df_test.loc[mask, "comment_text"].fillna("").tolist()
+        y_true = df_labels.loc[mask, LABELS].values
+    elif mode >= 3:
+        print("Invalid mode chosen. Please choose option 1 or 2.")
 
 df = pd.DataFrame(columns=headers)
 
@@ -70,10 +69,13 @@ if mode == 1:
 elif mode == 2:
     preds = []
     rows = []
-    threshold = 0.5
-    print(threshold)
-    for text in text_list:
+    threshold = 0.95
+
+    while mode == 2:
+        text = str(input("Please enter you model prompt"))
         inputs = tokenizer(text, padding=True, truncation=True, return_tensors="pt")
+
+        inputs = inputs.to(device)
 
         with torch.no_grad():
             outputs = model(**inputs)
@@ -83,13 +85,17 @@ elif mode == 2:
         binary_preds = (probs >= threshold).astype(int)
         preds.append(binary_preds)
 
-        row = [text] + probs.tolist()
+        row = [text] + binary_preds.tolist()
         rows.append(row)
 
+
+
+        print(text, binary_preds)
+        print("Would you like to exit the script?")
+        print("(1) Exit")
+        print("(2) New Prompt")
+        mode = int(input(":"))
+
     df = pd.DataFrame(rows, columns=["comment_text"] + LABELS)
-
-    df.to_csv('Outputs/output.csv', index=False)
-
-
 
 print("Model output.csv can be found in the Outputs folder")
